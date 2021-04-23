385a2e6a779599c41934b0604863ce8d0e6e2e0a
Elisha Hollander <just4now666666@gmail.com>
2021-04-23 14:14:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
-from setuptools import setup
-from itertools import product
 console_scripts = []
 for i in range(1, 4):
-    for j in product([chr(k) for k in range(97, 123)], repeat=i): console_scripts += ["".join(j) + "=random:seed"]
-setup(
+    for j in __import__('itertools').product([chr(k) for k in range(97, 123)], repeat=i): console_scripts += ["".join(j) + "=random:seed"]
+__import__('setuptools').setup(
     name='spamcmd',
     version='1.0.0',
     description='Delete every three letters command in the cmd using brute-force',
